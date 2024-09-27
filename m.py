dfd651d3350d6d6caf656c9fd466dda86d682ce6
sujoybarui22243yy6t7tttt5 <baruisujoy841@gmail.com>
2024-09-28 00:30:08 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E')
+bot = telebot.TeleBot('7448924519:AAFa3gfN4aMgTtsrbJHHg6K--wkAn2TjDCg')
 
 # Admin user IDs
-admin_id = ["5588464519"]
+admin_id = ["6590914935"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -307,8 +307,8 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert port to integer
             time = int(command[3])  # Convert time to integer
-            if time > 600:
-                response = "Error: Time interval must be less than 600."
+            if time > 900:
+                response = "Error: Time interval must be less than 900."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
